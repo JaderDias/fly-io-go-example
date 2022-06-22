4ece0baa350bc9ce8a2485dc795244320bb383c2
Jader Dias <52470+JaderDias@users.noreply.github.com>
2022-06-22 22:10:10 +0200

    add database connection

--- app.go
+++ app.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"database/sql"
 	"embed"
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/jackc/pgx/v4/stdlib"
 )
 
 //go:embed templates/*
@@ -21,10 +25,25 @@ func main() {
 	}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		data := map[string]string{
-			"Region": os.Getenv("FLY_REGION"),
+
+		db, err := sql.Open("pgx", os.Getenv("DATABASE_URL"))
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+			os.Exit(1)
+		}
+		defer db.Close()
+
+		var greeting string
+		err = db.QueryRow("select 'Hello, world!'").Scan(&greeting)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+			os.Exit(1)
 		}
 
+		data := map[string]string{
+			"Region":   os.Getenv("FLY_REGION"),
+			"Greeting": greeting,
+		}
 		t.ExecuteTemplate(w, "index.html.tmpl", data)
 	})
 
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/fly-apps/go-example
 
 go 1.16
+
+require github.com/jackc/pgx/v4 v4.16.1 // indirect
